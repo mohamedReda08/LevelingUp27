95c9c9b864b465e85a79f0ca2757437cb890e547
MohamedReda <mohamedreda@gmail.com>
2024-08-17 11:22:00 +0300

    Adding first basic test

---
+++ src/test/java/testPackage/BasicTest.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BasicTest {
+    @Test
+    public void basicTest(){
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.google.com/");
+            driver.quit();
+    }
+}
