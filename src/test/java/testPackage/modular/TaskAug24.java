9f2d2d683015ae92ebfe2b1f53351445a79b7e17
MohamedReda <mohamedreda@gmail.com>
2024-08-25 00:22:50 +0300

    Adding more tasks before POM design pattern

--- src/test/java/testPackage/modular/TaskAug24.java
+++ src/test/java/testPackage/modular/TaskAug24.java
@@ -11,7 +11,7 @@ import pages.SearchResultsPage;
 
 import java.time.Duration;
 
-public class Task24Aug {
+public class POMPractice {
      WebDriver driver;
      Wait<WebDriver> wait;
     LandingPage landingPage;
